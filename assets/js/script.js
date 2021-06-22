e7e42fb504140c90af44c1033ee13a100aab217b
Mat <mathew2801@gmail.com>
2021-06-22 13:28:53 +0000

    add a test div to hide game board ready to build a main menu before the game

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+
+function TestsFunction() {
+    document.getElementById("board").style.display = "";
+    document.getElementById("btn").style.display = "none";
+    document.getElementById("btnOne").style.display = "none";
+    document.getElementById("btnTwo").style.display = "none";
+}
