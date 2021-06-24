69b702f90533bfe0f97fb61cfd1e1d8d51990725
Mat <mathew2801@gmail.com>
2021-06-24 08:50:37 +0000

    add basic javascript for placement of x and circles to swap turns

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
-// selecting all 9 cell elements
+// creating variables
+const X_CLASS = 'x';
+const CIRCLE_CLASS = 'circle';
 let cellElements = document.querySelectorAll('[data-cell]');
+let circleTurn;
 // Once clicking on a cell it becomes true and can no longer be clicked
 cellElements.forEach(cell => {
     cell.addEventListener('click', handleClick, {once: true});
@@ -9,8 +12,27 @@ cellElements.forEach(cell => {
 This function will handle all click events
 */
 function handleClick(event) {
-    console.log('clicked')
+    const cell = event.target;
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+    placeMark(cell, currentClass);
+    //Check for win 
+    //Check for draw
+    switchTurns()
 };
+
+/*
+depending on whos turn it is this will place an x or a circle
+*/
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+}
+
+/*
+This will switch turns for each class x and circle
+*/
+function switchTurns() {
+    circleTurn = !circleTurn;
+}
 
 
 
