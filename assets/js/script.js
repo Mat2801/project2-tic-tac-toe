528979a69db71238a44a79e8c951521e5d48a126
Mat <mathew2801@gmail.com>
2021-06-24 10:35:27 +0000

    add winning combinations and a function to to check for a winner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
 // creating variables
 const X_CLASS = 'x';
 const CIRCLE_CLASS = 'circle';
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 let cellElements = document.querySelectorAll('[data-cell]');
 let board = document.getElementById('board');
 let circleTurn;
@@ -24,6 +34,9 @@ function handleClick(event) {
     const cell = event.target;
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
     placeMark(cell, currentClass);
+    if (checkWin(currentClass)) {
+        console.log("winner")
+    }
     //Check for win 
     //Check for draw
     switchTurns();
@@ -58,5 +71,13 @@ function setBoardHoverClass() {
 }
 }
 
-
-
+/* 
+check for a winning combination
+*/
+function checkWin(currentClass) {
+   return WINNING_COMBINATIONS.some(combination => {
+       return combination.every(index => {
+           return cellElements[index].classList.contains(currentClass)
+       });
+   });
+}
