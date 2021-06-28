8174e09cb9e27a3b900645df751a6678655dbea0
Mat <mathew2801@gmail.com>
2021-06-28 12:56:49 +0000

    edited main menu to just have play button leading to the game

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,18 +1,4 @@
 function playGameBtn() {
-    document.getElementById("play-ai").style.display = "";
-    document.getElementById("play").style.display = "none";
-    document.getElementById("how").style.display = "none";
-    document.getElementById("multiplayer").style.display = "none";
-}
-
-function howToPlayBtn() {
-    document.getElementById("how-to-play").style.display = "";
-    document.getElementById("play").style.display = "none";
-    document.getElementById("how").style.display = "none";
-    document.getElementById("multiplayer").style.display = "none";
-}
-
-function multiplayerBtn() {
     document.getElementById("board").style.display = "";
     document.getElementById("play").style.display = "none";
     document.getElementById("how").style.display = "none";
