a54c31ef2f8cf0177a9a68834997eb76e58b15c7
Mat <mathew2801@gmail.com>
2021-06-30 09:07:58 +0000

    remove style from html and onclick replaced with css style and onclick javascript functions

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,21 +1,26 @@
+let mainMenu = document.getElementById("mainMenu");
+let play = document.getElementById("play");
+let rulesArea = document.getElementById("rules");
+
+play.addEventListener("click", playGameBtn);
+
 function playGameBtn() {
-    document.getElementById("board").style.display = "";
-    document.getElementById("play").style.display = "none";
-    document.getElementById("how").style.display = "none";
+    menu.style.display = "none"
+    document.getElementById("board").style.display = "grid";
+
 }
 
+rulesArea.addEventListener("click", rulesBtn);
+
 function rulesBtn() {
-    document.getElementById("howToPlay").style.display = "";
-    document.getElementById("play").style.display = "none";
-    document.getElementById("how").style.display = "none";
+    menu.style.display = "none"
+    document.getElementById("howToPlay").style.display = "contents";
 }
+
+mainMenu.addEventListener("click", menuBtn);
 
 function menuBtn() {
     document.getElementById("menu").style.display = "";
-    document.getElementById("play").style.display = "";
     document.getElementById("howToPlay").style.display = "none";
-    document.getElementById("how").style.display = "none";
-
-
 }
 
